98f258e19c5f46e756d33fc2a2b1de3f910a7fce
Matt Barlow <matt.barlow@rackspace.com>
2014-04-22 17:20:14 -0500

    Fix URL parser

--- graphite_url_parser.py
+++ graphite_url_parser.py
@@ -4,23 +4,28 @@ import re
 def decode(my_url):
     my_url = urllib.unquote(my_url)
 
-    main_url = re.findall('^.*\?', my_url)[0]
-    graphite_functions = re.split('\?', my_url)[1]
-    graphite_functions = indent_parentheses(graphite_functions)
-    
-    separated_functions = re.split('\&', graphite_functions)
-    separated_functions.insert(0, main_url)
+    if re.search('\?', my_url):
 
-    return '\n'.join(separated_functions)
+        main_url = re.findall('^.*\?', my_url)[0]
+        graphite_functions = re.split('\?', my_url)[1]
+        graphite_functions = indent_parentheses(graphite_functions)
+        separated_functions = re.split('\&', graphite_functions)
+        for function in range(len(separated_functions) - 1):
+            separated_functions[function] = separated_functions[function] + '&'
+            
+        separated_functions.insert(0, main_url)
+
+        my_url = '\n'.join(separated_functions)
+
+    return my_url
 
 def encode(my_url):
 
     subbed_functions = unindent_parentheses(my_url)
-    subbed_functions = re.sub('\n', '&', subbed_functions)
-    subbed_functions = re.sub('\?\&', '?', subbed_functions)
-    subbed_functions = re.sub('\&$', '', subbed_functions)
+    subbed_functions = re.sub('\r', '', subbed_functions)
+    subbed_functions = re.sub('\n', '', subbed_functions)
     subbed_functions = re.sub(' *', '', subbed_functions)
-    return urllib.quote(subbed_functions, safe="/:=&?*()")
+    return urllib.quote(subbed_functions, safe="/:=&?*()\"")
 
 def indent_parentheses(my_url):
     stack = ""
@@ -32,6 +37,9 @@ def indent_parentheses(my_url):
         elif character == '(' and mycount >= 1:
             mycount = mycount + 1
             stack = stack + '\n' + mycount*' ' + '('
+        elif character == ')' and mycount >= 1:
+            mycount = mycount - 1
+            stack = stack + character
         else:
             stack = stack + character
 
