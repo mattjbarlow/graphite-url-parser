371c7d2e194db6b46fd92100e229f04be2a72ea0
Matt Barlow <matt.barlow@rackspace.com>
2014-04-21 19:54:53 -0500

    Add indentation for parentheses

--- graphite_url_parser.py
+++ graphite_url_parser.py
@@ -6,6 +6,7 @@ def decode(my_url):
 
     main_url = re.findall('^.*\?', my_url)[0]
     graphite_functions = re.split('\?', my_url)[1]
+    graphite_functions = indent_parentheses(graphite_functions)
     
     separated_functions = re.split('\&', graphite_functions)
     separated_functions.insert(0, main_url)
@@ -14,11 +15,29 @@ def decode(my_url):
 
 def encode(my_url):
 
-#    main_url = re.findall('^.*\?', my_url)[0]
-#    graphite_functions = re.split('\?', my_url)[1]
-
     subbed_functions = re.sub('\n', '&', my_url)
     subbed_functions = re.sub('\?\&', '?', subbed_functions)
     subbed_functions = re.sub('\&$', '', subbed_functions)
     return urllib.quote(subbed_functions, safe="/:=&?*")
 
+def indent_parentheses(my_url):
+    stack = ""
+    mycount = 0
+    for character in my_url:
+        if character == '(' and mycount == 0:
+            mycount = mycount + 1
+            stack = stack + character
+        elif character == '(' and mycount >= 1:
+            mycount = mycount + 1
+            stack = stack + '\n' + mycount*' ' + '('
+        else:
+            stack = stack + character
+
+    return stack
+            
+        
+            
+        
+        
+
+            
--- graphite_url_parser_test.py
+++ graphite_url_parser_test.py
@@ -3,12 +3,12 @@ import graphite_url_parser
 
 class GraphiteUrlParserTests(unittest.TestCase):
     def test_url_decoded_without_data_loss(self):
-        raw_url = """http://192.237.211.53/render?width=500&from=-2hours\
+        raw_url = """http://graphite/render?width=500&from=-2hours\
 &until=now&height=250&lineMode=connected&target=movingAverage%28\
 *.timers.tweetsearch.query.mean%2C5%29&title=Tweet.Search&\
 uniq=0.6707039752987425"""
 
-        parsed_url = """http://192.237.211.53/render?
+        parsed_url = """http://graphite/render?
 width=500
 from=-2hours
 until=now
@@ -23,7 +23,7 @@ uniq=0.6707039752987425"""
         
     def test_url_encoded_without_data_loss(self):
 
-        parsed_url = """http://192.237.211.53/render?
+        parsed_url = """http://graphite/render?
 width=500
 from=-2hours
 until=now
@@ -33,10 +33,32 @@ target=movingAverage(*.timers.tweetsearch.query.mean,5)
 title=Tweet.Search
 uniq=0.6707039752987425"""
 
-        raw_url = """http://192.237.211.53/render?width=500&from=-2hours\
+        raw_url = """http://graphite/render?width=500&from=-2hours\
 &until=now&height=250&lineMode=connected&target=movingAverage%28\
 *.timers.tweetsearch.query.mean%2C5%29&title=Tweet.Search&\
 uniq=0.6707039752987425"""
 
         transformed_url = graphite_url_parser.encode(parsed_url)
         self.assertEqual(transformed_url, raw_url)
+
+    def test_complex_url_decoded(self):
+
+        raw_url = """http://graphite.awesome.graphs.com/render?\
+width=400&from=-4hours&until=-&height=400&target=summarize\
+(sumSeries(stats.timers.accounts.*.http.post.accounts.response_time.count))\
+&drawNullAsZero=true&title=Example&\
+_uniq=0.11944825737737119"""
+
+        parsed_url = """http://graphite.awesome.graphs.com/render?
+width=400
+from=-4hours
+until=-
+height=400
+target=summarize(sumSeries
+  (stats.timers.accounts.*.http.post.accounts.response_time.count))
+drawNullAsZero=true
+title=Example
+_uniq=0.11944825737737119"""
+
+        transformed_url = graphite_url_parser.decode(raw_url)
+        self.assertEqual(transformed_url, parsed_url)
